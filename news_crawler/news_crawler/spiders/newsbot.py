e81ed203c626c6d931f52eed9cea9070db5d4e9c
one <imone075@gmail.com>
2023-06-16 18:57:42 +0900

    make spider can crawl links

--- news_crawler/news_crawler/spiders/newsbot.py
+++ news_crawler/news_crawler/spiders/newsbot.py
@@ -1,19 +1,20 @@
 import scrapy
 
 class NewsbotSpider(scrapy.Spider):
-    name = "newsbot"
-    allowed_domains = ["news.naver.com"]
-    start_urls = ["https://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=001"]
-
+    name = 'newsbot'
+    start_urls = ['http://news.naver.com/main/list.nhn?mode=LSD&mid=sec&sid1=103']
+    
     def parse(self, response):
         titles = response.xpath('//*[@id="main_content"]/div[2]/ul/li/dl/dt[2]/a/text()').extract()
+        links = response.xpath('//*[@id="main_content"]/div[2]/ul/li/dl/dt[2]/a/@href').extract()
         authors = response.css('.writing::text').extract()
         previews = response.css('.lede::text').extract()
         
-        for item in zip(titles, authors, previews):
+        for item in zip(titles, authors, links, previews):
             scraped_info = {
-                  'title' : item[0].strip(),
-                  'author' : item[1].strip(),
-                  'preview' : item[2].strip(),
-                  }
+				'title' : item[0].strip(),
+				'author' : item[1].strip(),
+				'link' : item[2].strip(),
+                'preview' : item[3].strip()
+			}
             yield scraped_info
