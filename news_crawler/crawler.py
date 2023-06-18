903e070e8092790e27535d6038a245faafef91d8
one <imone075@gmail.com>
2023-06-18 23:01:13 +0900

    few test

--- news_crawler/crawler.py
+++ news_crawler/crawler.py
@@ -1,7 +1,9 @@
 import os
+import scrapy
 
-os.system("conda activate mlenv")
-os.system("cd news_crawler/spiders")
-with open("naver_news.json", 'r+') as f: f.truncate(0)
-os.system("scrapy crawl newsbot")
-os.system("cat naver_news.json")
+os.system("conda activate mlenv") # activate mlenv
+os.system("cd news_crawler/spiders") # move to spider
+with open("naver_news.json", 'r+') as f: f.truncate(0) # initialize json file
+os.system("scrapy crawl newsbot") # crawl with scrapy
+os.system("cat naver_news.json") # output crawled data
+
